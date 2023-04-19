93f6ec52f7064cbe665c0b99c5b2a0f751dd93f3
laniakea <liza.xtbtds@gmail.com>
2023-04-19 10:37:11 +0200

    first

---
+++ app/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from config import settings
+
+
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# an instance of the SessionLocal will be database session
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# inherit from this class to create each of the database models or classes (the ORM models)
+Base = declarative_base()
---
+++ app/models.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    surname = Column(String)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+    posts = relationship("Post", back_populates="owner")
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    content = Column(String)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="posts")
+
+# class Group(Base):
+#     __tablename__ = "groups"
+#     id = Column(Integer, primary_key=True, index=True)
+#     name = Column(String)
